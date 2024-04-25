c8bc6b707e1c3d52a1853d19c3d408dd56eac7d5
falom25 <165881691+falom25@users.noreply.github.com>
2024-04-25 11:29:40 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-ff4348cb-a989-4624-9122-21641f803de1.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-fd018e93-07e9-4f16-bfb0-4759eb89a8f5.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
